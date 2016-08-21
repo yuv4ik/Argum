85cc14f3a7b6e45a09cae49647f4f3a8dfe62baf
Evgeny Zborovsky <yuv4ik@gmail.com>
2016-08-21 15:06:29 +0300

    V1.0.0.0

---
+++ Argum/ArgumExtractor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Argum
+{
+    /// <summary>
+    /// <para>Responsible for extracting arguments from given input.</para>
+    /// </summary>
+    public class ArgumExtractor
+    {
+
+        private string[] input;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="args">Expects the input in the next format: --arg1=val1 --arg2=val2</param>
+        public ArgumExtractor(string[] args)
+        {
+            input = args;
+        }
+
+        /// <summary>
+        /// <para>Returns an argument value with appropriate type.</para>
+        /// <para>Throws ArgumException.</para>
+        /// </summary>
+        /// <typeparam name="T">Output type</typeparam>
+        /// <param name="arg">Argument key</param>
+        /// <returns></returns>
+        public T GetArgument<T>(string arg)
+        {
+            try
+            {
+                var argKey = $"--{arg}=";
+                var argVal = input.First(x => x.StartsWith(argKey));
+                return (T)Convert.ChangeType(argVal.Replace(argKey, string.Empty), typeof(T));
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumException($"GetArgument {arg} is failed with:", ex);
+            }
+        }
+    }
+
+    public class ArgumException : Exception
+    {
+        public ArgumException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
+
---
+++ ArgumTests/ArgumTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Argum.Tests
+{
+    [TestClass()]
+    public class ArgumTests
+    {
+        private IDictionary<string, object> inputArgs = new Dictionary<string, object>
+        {
+            { "string", "one" },
+            { "int", 1 },
+            { "double", 1.3 },
+            { "bool", bool.TrueString },
+            { "dateTime", DateTime.Parse("02/09/1988") },
+            { "float", 1.3f }
+        };
+
+        [TestMethod()]
+        public void GetStringArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+            var argKey = "string";
+
+            // Act
+            var res = argum.GetArgument<string>(argKey);
+
+            // Assert
+            Assert.AreEqual(res, inputArgs[argKey]);
+        }
+
+        [TestMethod()]
+        public void GetIntArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+            var argKey = "int";
+
+            // Act
+            var res = argum.GetArgument<int>(argKey);
+
+            // Assert
+            Assert.AreEqual(res, inputArgs[argKey]);
+        }
+
+        [TestMethod()]
+        public void GetDoubleArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+            var argKey = "double";
+
+            // Act
+            var res = argum.GetArgument<double>(argKey);
+
+            // Assert
+            Assert.AreEqual(res, inputArgs[argKey]);
+        }
+
+        [TestMethod()]
+        public void GetBooleanArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+
+            // Act
+            var res = argum.GetArgument<bool>("bool");
+
+            // Assert
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod()]
+        public void GetDateTimeArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+            var argKey = "dateTime";
+
+            // Act
+            var res = argum.GetArgument<DateTime>(argKey);
+
+            // Assert
+            Assert.AreEqual(res, inputArgs[argKey]);
+        }
+
+        [TestMethod()]
+        public void GeFloatArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+            var argKey = "float";
+
+            // Act
+            var res = argum.GetArgument<float>(argKey);
+
+            // Assert
+            Assert.AreEqual(res, inputArgs[argKey]);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumException))]
+        public void GetUnknowsArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+
+            // Act
+            var res = argum.GetArgument<bool>("non-existing-element");
+
+            // Assert
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumException))]
+        public void GetWrongTypeArgumentTest()
+        {
+            // Arrange
+            var argum = GenerateArgumExtractor(GenerateInput());
+
+            // Act
+            var res = argum.GetArgument<bool>("int");
+
+            // Assert
+        }
+
+        private string[] GenerateInput()
+        {
+            var result = new List<string>();
+            inputArgs.ToList().ForEach(p => result.Add($"--{p.Key}={p.Value}"));
+
+            return result.ToArray();
+        }
+
+        private ArgumExtractor GenerateArgumExtractor(string[] args) => new ArgumExtractor(args);
+    }
+}
